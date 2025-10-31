db7230327b1f1cefc15bfcb2b6aef7652e8664f4
coolboy67yt <coolboy67yt@gmail.com>
2025-10-30 21:19:53 -0500

    add verified stuff and more idfk im tired

--- assets/js/profiles.js
+++ assets/js/profiles.js
@@ -5,11 +5,19 @@ const profileList = document.getElementById('profileList');
 // simple. just grab the user list and then add the profile card.
 users.forEach(user => {
     const card = document.createElement('div');
+    let verified = '<div class="tooltip" data-tooltip="Unverified" style="border-radius: 40%; padding: 2px; padding-bottom: 6px; width: 10px; text-align: center; height: 18px;"></div>'
     card.className = 'profile-card';
+    if (user.verified === true) {
+        verified = `
+            <div class="tooltip" data-tooltip="Verified" style="border-radius: 40%; padding: 2px; padding-bottom: 6px; width: 18px; text-align: center; height: 18px;">
+                <span style="font-size: 15px; font-family: 'Miiverse Symbols'; color: limegreen;">v</span>
+            </div>
+        `;
+    }
     card.innerHTML = `
         <img src="${user.profilePic}" alt="${user.name}">
         <div class="profile-info">
-            <a href="${user.youtube}" target="_blank">${user.name}</a><span class="username">(${user.username})</span>
+            <a href="${user.youtube}" target="_blank">${user.name}</a>${verified}<span class="username">(${user.username})</span>
             <p>${user.description}</p>
         </div>
     `;
